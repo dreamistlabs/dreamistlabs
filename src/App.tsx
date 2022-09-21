2417c5902397cbc0f76d2b2d496e52d3aac66bc8
Johnny Hu <johnny@dreamistlabs.com>
2022-09-21 17:29:26 +0100

    Feature: Initial release of website with placeholder landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { SxProps } from '@mui/material';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './App.css';
+import pkg from '../package.json';
 
-function App() {
+const styles = {
+  header: {
+    display: 'flex',
+    padding: '4px 8px',
+    width: '100%',
+  } as SxProps,
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  } as SxProps,
+  footer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    padding: '4px 8px',
+    width: '100%',
+  } as SxProps,
+};
+
+const App = (): React.ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+    <Box className="App">
+      <Box component="header" sx={styles.header}>
+        <Typography sx={{ fontWeight: 500 }}>DreamistLabs</Typography>
+      </Box>
+      <Box sx={styles.content}>
+        <Typography color="text.secondary" sx={{ fontWeight: 500 }} variant="h4">
+          One Developer, Lots of Code
+        </Typography>
+        <Typography color="text.secondary" sx={{ fontStyle: 'italic' }} variant="body1">
+          (all experimental)
+        </Typography>
+        <IconButton
+          aria-label="Go to Dreamistlabs on GitHub"
+          href="https://github.com/dreamistlabs"
           rel="noopener noreferrer"
+          size="large"
+          sx={{ margin: 2 }}
+          target="_blank"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <GitHubIcon fontSize="inherit" />
+        </IconButton>
+      </Box>
+      <Box component="footer" sx={styles.footer}>
+        <Typography variant="caption">v{pkg.version}</Typography>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
